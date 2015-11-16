9a445a2b0ad309ad75807596ac363476e1f40397
Rafael Oliveira <rafaelhenrique.oliveira7@gmail.com>
2015-11-16 17:10:24 -0200

    Definindo diretório de trabalho

---
+++ capitulo3/docker_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DockerExample::Application.config.secret_token = '8b437cbcc759c8e4018478fb8bc0eca42683a0209669c1312ab688febef7162efc92973ddd1f7961dda5ae4a634d8e41a7a800f228a3ede3a6e375bb9ec62c5f'
